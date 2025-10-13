3b3c5d5726221081e494adc17df375ea040268c4
yzoo0112-byte <yzoo0112@gmail.com>
2025-10-13 17:49:06 +0900

    사이드바 초안

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Routes } from "react-router-dom";
+import { SideNav } from "./mater/SideNav";
 
 function App() {
   return (
     <BrowserRouter>
+    <SideNav/>
       <Routes>
         {/* <Route path="/board/show/:category" element={< >} /> */}
       </Routes>
