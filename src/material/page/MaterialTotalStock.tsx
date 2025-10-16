468ce1a5e397e8b1d58215bf7078db6cf419845f
chwolog1115 <chwolog1115@gmail.com>
2025-10-16 17:53:11 +0900

    Update MaterialTotalStock.tsx

--- src/material/page/MaterialTotalStock.tsx
+++ src/material/page/MaterialTotalStock.tsx
@@ -1,3 +1,35 @@
+import { useGridApiRef, type GridColDef } from "@mui/x-data-grid";
+
 export function MaterialTotalStock() {
+  const apiRef = useGridApiRef();
+  const sampleData = [
+    "회사1",
+    "회사2",
+    "품목A",
+    "품목B",
+    "입고번호001",
+    "입고번호002",
+  ];
+  const searchOptions = [
+    { label: "매입처명", value: "companyName" },
+    { label: "품목번호", value: "materialCode" },
+    { label: "품목명", value: "materialName" },
+  ];
+  const columns: GridColDef[] = [
+    {
+      field: "inNum",
+      headerName: "입고번호",
+      width: 150,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "materialName",
+      headerName: "품목명",
+      width: 150,
+      headerAlign: "center",
+      align: "center",
+    },
+  ];
   return <></>;
 }
