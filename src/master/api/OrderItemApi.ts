ddeb8ab818224b5aff04672431ddc1a2772a3c3e
yzoo0112-byte <yzoo0112@gmail.com>
2025-10-17 16:14:31 +0900

    이미지 등록 작성 중

--- src/master/api/OrderItemApi.ts
+++ src/master/api/OrderItemApi.ts
@@ -10,12 +10,36 @@ export const getMasterOrItList = async (): Promise<MasterOrItList[]> => {
   return response.data;
 };
 
+// export const registerOrderItem = async (data: MasterOrItRegister) => {
+//   const response = await axios.post(
+//     `${BASE_URL}/master/orderitem/register`,
+//     data
+//   );
+//   return response.data;
+// };
+
 export const registerOrderItem = async (data: MasterOrItRegister) => {
-  const response = await axios.post(
-    `${BASE_URL}/master/orderitem/register`,
-    data
-  );
-  return response.data;
+  const formData = new FormData();
+
+  // JSON 데이터 따로 추가
+  const json = JSON.stringify({
+    itemCode: data.itemCode,
+    itemName: data.itemName,
+    company: data.company,
+    type: data.type,
+    unitPrice: data.unitPrice,
+    color: data.color,
+    coatingMethod: data.coatingMethod,
+    remark: data.remark,
+  });
+  formData.append("data", new Blob([json], { type: "application/json" }));
+
+  // 파일 추가
+  data.imgUrl.forEach((file) => formData.append("imgUrl", file));
+
+  return axios.post(`${BASE_URL}/master/orderitem/register`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
 };
 
 export const updateOrItState = async (id: number, updatedState: string) => {
@@ -28,7 +52,6 @@ export const updateOrItState = async (id: number, updatedState: string) => {
 
 export const getOrItDetail = async (id: number) => {
   const response = await axios.get(`${BASE_URL}/master/orderitem/detail/${id}`);
-  console.log(response.data);
   return response.data;
 };
 
