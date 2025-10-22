79bcb441538ab74dbf1ef72fd2a11d2574c20a7d
yzoo0112-byte <yzoo0112@gmail.com>
2025-10-22 16:32:36 +0900

    사이드바 텍스트

--- src/common/SideNav.tsx
+++ src/common/SideNav.tsx
@@ -31,7 +31,7 @@ export function SideNav() {
     >
       <SidebarItems>
         <SidebarItemGroup>
-          <SidebarItem href="/" icon={HiChartPie}>
+          <SidebarItem href="/" icon={HiChartPie} style={{ color: "black" }}>
             대시보드
           </SidebarItem>
           <SidebarCollapse
@@ -39,24 +39,31 @@ export function SideNav() {
             label="수주대상 입출고 관리"
             open
           >
-            <SidebarItem href="/" icon={RiInboxArchiveLine}>
+            <SidebarItem
+              href="/"
+              icon={RiInboxArchiveLine}
+              style={{ color: "black" }}
+            >
               입고 등록
             </SidebarItem>
             <SidebarItem
               href="/orderitem/inbound/list"
               icon={RiInboxArchiveFill}
+              style={{ color: "black" }}
             >
               입고 조회
             </SidebarItem>
             <SidebarItem
               href="/orderitem/outbound/register"
               icon={RiInboxUnarchiveLine}
+              style={{ color: "black" }}
             >
               출고 등록
             </SidebarItem>
             <SidebarItem
               href="/orderitem/outbound/list"
               icon={RiInboxUnarchiveFill}
+              style={{ color: "black" }}
             >
               출고 조회
             </SidebarItem>
@@ -65,47 +72,65 @@ export function SideNav() {
             <SidebarItem
               href="/material/inbound/register"
               icon={RiInboxArchiveLine}
+              style={{ color: "black" }}
             >
               입고 등록
             </SidebarItem>
             <SidebarItem
               href="/material/inbound/list"
               icon={RiInboxArchiveFill}
+              style={{ color: "black" }}
             >
               입고 조회
             </SidebarItem>
             <SidebarItem
               href="/material/outbound/register"
               icon={RiInboxUnarchiveLine}
+              style={{ color: "black" }}
             >
               출고 등록
             </SidebarItem>
             <SidebarItem
               href="/material/outbound/list"
               icon={RiInboxUnarchiveFill}
+              style={{ color: "black" }}
             >
               출고 조회
             </SidebarItem>
-            <SidebarItem href="/material/totalstock" icon={FaBoxArchive}>
+            <SidebarItem
+              href="/material/totalstock"
+              icon={FaBoxArchive}
+              style={{ color: "black" }}
+            >
               재고 현황
             </SidebarItem>
           </SidebarCollapse>
           <SidebarCollapse icon={HiShoppingBag} label="기준 정보 관리" open>
-            <SidebarItem href="/master/orderitem/list" icon={HiClipboard}>
+            <SidebarItem
+              href="/master/orderitem/list"
+              icon={HiClipboard}
+              style={{ color: "black" }}
+            >
               수주대상품목
             </SidebarItem>
-            <SidebarItem href="/master/material/list" icon={HiCube}>
+            <SidebarItem
+              href="/master/material/list"
+              icon={HiCube}
+              style={{ color: "black" }}
+            >
               원자재품목
             </SidebarItem>
             <SidebarItem
               href="/master/routing/list"
               icon={HiOutlineArrowsUpDown}
+              style={{ color: "black" }}
             >
               라우팅
             </SidebarItem>
             <SidebarItem
               href="/master/company/list"
               icon={HiMiniBuildingLibrary}
+              style={{ color: "black" }}
             >
               업체
             </SidebarItem>
